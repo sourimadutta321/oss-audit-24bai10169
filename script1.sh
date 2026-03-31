5e411f9591d6e57b1416709b909395f24d2a7932
sourimadutta321 <sourimadutta321@gmail.com>
2026-03-31 23:02:45 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@
 # Script 1: System Identity Report
 # Author: Your Name | Course: Open Source Software
 
-STUDENT_NAME="Shivansh"
+STUDENT_NAME="Sourima"
 SOFTWARE_CHOICE="Git"
 
 KERNEL=$(uname -r)
